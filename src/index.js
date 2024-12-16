c0dc53a6aecf9b5bea8608d7faf5ceb5faed4040
Stellakoomson <stellakoomson006@gmail.com>
2024-12-16 13:55:01 +0000

    adding all changes

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//let x = prompt("give me a number")
+//console.log(typeof(x));
+//x = Number
+
+//if (condition){}
+
+//let  Del = prompt("Are ypu sure you want to delete ")
+//if (Del = "yes");
+//alert("account deledted")
+
+
+let booking = prompt("would you like to confirm your appointment") 
+if (booking == "confirm");
+alert('booking confirmed')
+
+elseif (booking == "cancel");
+alert('booking canceled')
+
+
+
